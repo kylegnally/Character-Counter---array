6974caa2f077ceaa01d045a049bb968ff887ed72
Kyle G. Nally <kylegnally@hotmail.com>
2021-04-04 14:18:50 -0400

    Fixed final filename and output formatting.

--- Character Counter - array/Program.cs
+++ Character Counter - array/Program.cs
@@ -7,7 +7,7 @@
 #if DEBUG
             //  use the args below to specify output text filename once that's implemented
             //args = new[] { "wap.txt", "out.txt" };
-            args = new[] { "test.txt" };
+            args = new[] { "wp.txt" };
             #endif
             CounterManager capp = new CounterManager(args);
         }
